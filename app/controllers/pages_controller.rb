de50b54dd9219133a6a3289870265d91c0c0f8d8
Jocelyn Proctor <jocelynkproctor@gmail.com>
2020-07-13 17:01:47 +0200

    created index and show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
-  def home; end
+  def home
+    @items = Item.all.reverse
+  end
+
+  def swaped; end
 
   def components; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "components", to: 'pages#components'
+  get "swaped", to: 'pages#swaped'
 
   resources :items
 end
