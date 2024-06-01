67853c300df7739291a6b2a62c64a60ffd4776c9
Jocelyn Proctor <jocelynkproctor@gmail.com>
2024-06-01 19:05:22 +0200

    updated dependencies

--- Gemfile
+++ Gemfile
@@ -60,7 +60,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
-  gem 'webdrivers'
+  gem 'webdrivers', '= 5.3.0'
   gem 'shoulda-matchers', '~> 4.0'
   gem 'factory_bot_rails', '~> 6.1'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Allow requests from these domains and their subdomains
+  config.hosts << "swap-it.captain.sudete.de"
+  config.hosts << "swap-it.sudete.de"
+  config.hosts << "swap-it.proctor-webworks.com"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
